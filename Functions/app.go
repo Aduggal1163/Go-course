8f562f03f96b926687cd0db5cd7ac5c43ef59cd3
Aduggal1163 <abhiduggal1163@gmail.com>
2026-03-20 12:48:10 +0530

    Add initial implementation of app and profit calculation functions

---
+++ Functions/app.go
@@ -0,0 +1,46 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	greet()
+// 	var name string
+// 	greetUser(name)
+// 	var city string
+// 	var month int
+// 	a, b := get_cityAndmonth(city, month)
+// 	fmt.Println("City is: ", a, "\nMonth is: ", b)
+// 	var l int
+// 	var w int
+// 	area := calculatearea(l, w)
+// 	fmt.Println(area)
+// 	fmt.Println(add(30, 5))
+// }
+
+// func greet() {
+// 	fmt.Println("Hello")
+// }
+
+// func greetUser(name string) {
+// 	name = "Abhishek"
+// 	fmt.Println("Hello", name)
+// }
+
+// func get_cityAndmonth(city string, month int) (string, int) {
+// 	city = "Nabha"
+// 	month = 3
+// 	return city, month
+// }
+
+// func calculatearea(l int, w int) (area int) {
+// 	l = 10
+// 	w = 10
+// 	area = l * w
+// 	return
+// }
+
+// func add(a, b int) int {
+// 	return a + b
+// }
---
+++ Functions/profit.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+	outputText("Enter revenue: ")
+	fmt.Scan(&revenue)
+	outputText("Enter expenses: ")
+	fmt.Scan(&expenses)
+	outputText("Enter tax ratio: ")
+	fmt.Scan(&taxRate)
+	EBT := calculateEBT(revenue, expenses)
+	profit := calculateProfit(EBT, taxRate)
+	ratio := EBT / profit
+	fmt.Printf("ebt is : %.2f \nprofit is : %.2f \nratio is : %.2f ", EBT, profit, ratio)
+}
+func outputText(title string) {
+	fmt.Print(title)
+}
+func calculateEBT(revenue float64, expenses float64) float64 {
+	return revenue - expenses
+}
+func calculateProfit(EBT float64, taxRate float64) (sol float64) {
+	sol = EBT * (1 - taxRate/100)
+	return
+}
